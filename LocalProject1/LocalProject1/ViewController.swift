210cdf19752fdd05a572b43e15347daf99e7b874
gopalsvaid <vaidgopal@gmail.com>
2026-02-04 13:12:26 +0530

    Add initial files

---
+++ LocalProject1/LocalProject1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocalProject1
+//
+//  Created by Gopal Vaid on 04/02/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LocalProject1/LocalProject1Tests/LocalProject1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  LocalProject1Tests.swift
+//  LocalProject1Tests
+//
+//  Created by Gopal Vaid on 04/02/26.
+//
+
+import Testing
+@testable import LocalProject1
+
+struct LocalProject1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
